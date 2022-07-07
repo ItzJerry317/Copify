bcdc7d2e2cbfb630b3537e7ad0d5a9a328b4d5d9
ItzJerry317 <85010676+ItzJerry317@users.noreply.github.com>
2022-07-07 18:45:03 +0800

    Change

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const {app, BrowserWindow} = electron;
 
 let mainWindow;
 
 // Listen for app to be ready
 app.on('ready', function(){
+  console.log('App is ready');
   // Create new window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 600,
+    resizable: false,
+  });
   // Load html into window
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'UI/index.html'),
     protocol: 'file:',
     slashes: true
   }));
+  //remove menu
+  mainWindow.setMenu(null);
   // Quit app when closed
   mainWindow.on('closed', function(){
     app.quit();
